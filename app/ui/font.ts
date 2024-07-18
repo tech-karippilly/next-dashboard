afbd95945ceab3677dd2ad88b969ca528f7dfc22
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-07-18 17:51:32 +0530

    dashboard app

---
+++ app/ui/font.ts
@@ -0,0 +1,7 @@
+import { Inter ,Lusitana} from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lustania =Lusitana({
+    weight:['400','700'],
+    subsets:['latin']
+})
